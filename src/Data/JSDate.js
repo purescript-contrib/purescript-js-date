05f094f41bf0c338e4fdaa40a94f38f3caf07451
Thomas R. Honeyman <thoneyman@awakesecurity.com>
2020-03-04 21:32:43 -0800

    Fix comment format in JSDate.js and run formatter

--- src/Data/JSDate.js
+++ src/Data/JSDate.js
@@ -1,7 +1,7 @@
-/* global exports */
+// global exports
 "use strict";
 
-var createDate = function (y, m, d, h, mi, s, ms) {
+var createDate = function(y, m, d, h, mi, s, ms) {
   var date = new Date(Date.UTC(y, m, d, h, mi, s, ms));
   if (y >= 0 && y < 100) {
     date.setUTCFullYear(y);
@@ -9,7 +9,7 @@ var createDate = function (y, m, d, h, mi, s, ms) {
   return date;
 };
 
-var createLocalDate = function (y, m, d, h, mi, s, ms) {
+var createLocalDate = function(y, m, d, h, mi, s, ms) {
   var date = new Date(y, m, d, h, mi, s, ms);
   if (y >= 0 && y < 100) {
     date.setFullYear(y);
@@ -17,53 +17,69 @@ var createLocalDate = function (y, m, d, h, mi, s, ms) {
   return date;
 };
 
-exports.now = function () {
+exports.now = function() {
   return new Date();
 };
 
-exports.isValid = function (date) {
+exports.isValid = function(date) {
   return !isNaN(date.getTime());
 };
 
-exports.toInstantImpl = function (just) {
-  return function (nothing) {
-    return function (date) {
+exports.toInstantImpl = function(just) {
+  return function(nothing) {
+    return function(date) {
       var t = date.getTime();
       return isNaN(t) ? nothing : just(t);
     };
   };
 };
 
-exports.fromInstant = function (instant) {
+exports.fromInstant = function(instant) {
   return new Date(instant);
 };
 
-exports.jsdate = function (parts) {
-  return createDate(parts.year, parts.month, parts.day, parts.hour, parts.minute, parts.second, parts.millisecond);
+exports.jsdate = function(parts) {
+  return createDate(
+    parts.year,
+    parts.month,
+    parts.day,
+    parts.hour,
+    parts.minute,
+    parts.second,
+    parts.millisecond
+  );
 };
 
-exports.jsdateLocal = function (parts) {
-  return function () {
-    return createLocalDate(parts.year, parts.month, parts.day, parts.hour, parts.minute, parts.second, parts.millisecond);
+exports.jsdateLocal = function(parts) {
+  return function() {
+    return createLocalDate(
+      parts.year,
+      parts.month,
+      parts.day,
+      parts.hour,
+      parts.minute,
+      parts.second,
+      parts.millisecond
+    );
   };
 };
 
-exports.dateMethod = function (method, date) {
+exports.dateMethod = function(method, date) {
   return date[method]();
 };
 
-exports.dateMethodEff = function (method, date) {
-  return function () {
+exports.dateMethodEff = function(method, date) {
+  return function() {
     return date[method]();
   };
 };
 
-exports.parse = function (dateString) {
-  return function () {
+exports.parse = function(dateString) {
+  return function() {
     return new Date(dateString);
   };
 };
 
-exports.fromTime = function (time) {
+exports.fromTime = function(time) {
   return new Date(time);
 };
